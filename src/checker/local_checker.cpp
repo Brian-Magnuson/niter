04111f3dbda489e1b371e4561093b34d184ba4db
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2024-05-09 11:27:32 -0400

    Add dummy implementations for local_checker

--- src/checker/local_checker.cpp
+++ src/checker/local_checker.cpp
@@ -2,6 +2,117 @@
 #include "../logger/logger.h"
 #include <iostream>
 
+std::any LocalChecker::visit_declaration_stmt(Stmt::Declaration* /* stmt */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_expression_stmt(Stmt::Expression* /* stmt */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_block_stmt(Stmt::Block* /* stmt */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_conditional_stmt(Stmt::Conditional* /* stmt */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_loop_stmt(Stmt::Loop* /* stmt */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_return_stmt(Stmt::Return* /* stmt */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_break_stmt(Stmt::Break* /* stmt */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_continue_stmt(Stmt::Continue* /* stmt */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_print_stmt(Stmt::Print* /* stmt */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_eof_stmt(Stmt::EndOfFile* /* stmt */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+// var_decl, fun_decl
+std::any LocalChecker::visit_var_decl(Decl::Var* /* decl */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_fun_decl(Decl::Fun* /* decl */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_assign_expr(Expr::Assign* /* expr */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_logical_expr(Expr::Logical* /* expr */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_binary_expr(Expr::Binary* /* expr */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_unary_expr(Expr::Unary* /* expr */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_call_expr(Expr::Call* /* expr */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_access_expr(Expr::Access* /* expr */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_identifier_expr(Expr::Identifier* /* expr */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any LocalChecker::visit_literal_expr(Expr::Literal* /* expr */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any visit_array_expr(Expr::Array* /* expr */) {
+    // TODO: Implement this
+    return std::any();
+}
+
+std::any visit_tuple_expr(Expr::Tuple* /* expr */) {
+    // TODO: Implement this
+    return std::any();
+}
+
 void LocalChecker::type_check(std::vector<std::shared_ptr<Stmt>> stmts) {
     for (auto stmt : stmts) {
         try {
