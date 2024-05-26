b26d3ccf212f494e9dc4d369983882614d20a735
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2024-05-26 14:27:43 -0400

    Add test for undeclared variables

--- test/checker_fun_test.cpp
+++ test/checker_fun_test.cpp
@@ -931,3 +931,36 @@ fun main(): i32 {
     logger.reset();
     env.reset();
 }
+
+TEST_CASE("Local checker undeclared variable", "[checker]") {
+    std::string source_code = R"(
+fun main(): i32 {
+    a = 1;
+    return 0;
+}
+)";
+    auto file_name = std::make_shared<std::string>("test_files/undeclared_variable.nit");
+
+    ErrorLogger& logger = ErrorLogger::inst();
+    logger.set_printing_enabled(false);
+
+    Scanner scanner;
+    scanner.scan_file(file_name, std::make_shared<std::string>(source_code));
+
+    Parser parser(scanner.get_tokens());
+    auto stmts = parser.parse();
+
+    Environment& env = Environment::inst();
+
+    GlobalChecker global_checker;
+    global_checker.type_check(stmts);
+
+    LocalChecker local_checker;
+    local_checker.type_check(stmts);
+
+    REQUIRE(logger.get_errors().size() >= 1);
+    CHECK(logger.get_errors().at(0) == E_UNKNOWN_VAR);
+
+    logger.reset();
+    env.reset();
+}
